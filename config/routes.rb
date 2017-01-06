ffd46baa9d610d5cfb49463b58c0c0cba158b33d
Ken Berard <savethewhalesharks3@gmail.com>
2017-01-06 03:48:22 +0000

    Cleaned up contact form routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
-  resources :contacts
+  resources :contacts, only: :create
   get 'tricks', to: 'pages#tricks'
-  get 'contact-me', to: 'contacts#new'
+  get 'contact-me', to: 'contacts#new', as: 'new_contact'
 end
