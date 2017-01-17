672515d958a9d8b3f90eaa3634105e02782021d3
Ken Berard <savethewhalesharks3@gmail.com>
2017-01-17 03:58:11 +0000

    Added profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,9 @@
 class ProfilesController < ApplicationController
   
+  # GET to /users/:user_id/profile_new
+  def new
+    # Render blank profile details form
+    @profile = Profile.new
+  end
+  
 end
